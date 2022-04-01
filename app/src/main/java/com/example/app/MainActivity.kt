106347475655f27f9f906d7ae6eaabbd6be8c9da
홍채민 <coals0329@cocen.com>
2022-04-01 14:50:44 +0900

    feat(sample): lamda

--- app/src/main/java/com/example/app/MainActivity.kt
+++ app/src/main/java/com/example/app/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 5. 내부 익명 함수
+        val button = findViewById<Button>(R.id.button);
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0 : View?) {
+                // TODO...
+            }
+        });
+
+        // 내부 익명 함수를 사용하기 위한 조건
+        // 1. kotlin.interface 가 아닌 java interface 여야 한다
+        // 2. 그 interface 는 딱 하나이 메소드만 가져야 한다
+
+        button.setOnClickListener {
+            // TODO...
+        };
     }
+
 }
