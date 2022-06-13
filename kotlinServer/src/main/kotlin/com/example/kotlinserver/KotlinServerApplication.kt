351cad7b69193942fa0c82cc38814989156128cd
홍채민 <coals0329@cocen.com>
2022-06-13 15:45:52 +0900

    feat(test): util? extenstion?

--- kotlinServer/src/main/kotlin/com/example/kotlinserver/KotlinServerApplication.kt
+++ kotlinServer/src/main/kotlin/com/example/kotlinserver/KotlinServerApplication.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinserver
 
+import com.example.kotlinserver.extensions.toSlug
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -8,4 +9,5 @@ class KotlinServerApplication
 
 fun main(args: Array<String>) {
     runApplication<KotlinServerApplication>(*args)
+    println("HELLO 2022-06-13T06:41:35.828+00:00".toSlug()) // hello-2022-06-13t06-41-35-828-00-00
 }
--- kotlinServer/src/main/kotlin/com/example/kotlinserver/TestController.kt
+++ kotlinServer/src/main/kotlin/com/example/kotlinserver/TestController.kt
@@ -1,11 +1,11 @@
 package com.example.kotlinserver
 
-import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class TestController {
 
     @GetMapping("/test")
