95ec6dfde5ef75f490975f282d1ce6bcdc2db787
Hannah <ghdcoalss33@gmail.com>
2022-08-13 19:32:13 +0900

    feat(do-it-kotlin): chapter 6 프로퍼티와 초기화

---
+++ HelloKotlin/src/main/kotlin/chpater6/ObjectExpressionSuperMan.kt
@@ -0,0 +1,17 @@
+package chpater6
+
+open class Superman() {
+    fun work() = println("Taking photos")
+    fun talk() = println("Talking with people.")
+    open fun fly() = println("Flying in the air.")
+}
+
+fun main() {
+    val pretendedMan = object: Superman() { // ① object 표현식으로 fly() 구현의 재정의
+        override fun fly() = println("I'm not a real superman. I can't fly!")
+    }
+
+    pretendedMan.work()
+    pretendedMan.talk()
+    pretendedMan.fly()
+}
