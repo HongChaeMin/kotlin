2e9bc84077d7721f928cf4027bc7ed4df18677ef
Hannah <ghdcoalss33@gmail.com>
2022-08-26 23:25:44 +0900

    feat(do-it-kotlin): chapter8 upload

---
+++ HelloKotlin/src/main/kotlin/chapter8/GenericNull.kt
@@ -0,0 +1,19 @@
+package chapter8
+
+class GenericNull<T> { // 기본적으로 null이 허용되는 형식 매개 변수
+    // null을 허용하지 않게 하려면 <T: Any> 로 선언하면 된다
+
+    fun equalityFunc(arg1: T, arg2: T) {
+        println(arg1?.equals(arg2))
+    }
+}
+
+fun main() {
+
+    val obj = GenericNull<String>() // non-null로 선언됨
+    obj.equalityFunc("Hello", "World") // null이 허용되지 않음
+
+    val obj2 = GenericNull<Int?>() // null이 가능한 형식으로 선언됨
+    obj2.equalityFunc(null, 10)
+}
+
---
+++ HelloKotlin/src/main/kotlin/chapter8/LambdaParameter.kt
@@ -0,0 +1,16 @@
+package chapter8
+
+fun <T> add(a: T, b: T, op: (T, T) -> T): T {
+    return op(a, b)
+}
+
+fun main() {
+    val result = add(2, 3) { a, b -> a + b }
+    println(result)
+
+    println(add(2, 3, sumInt))
+    println(add(2, 3, sumInt2))
+}
+
+var sumInt: (Int, Int) -> Int = { a, b -> a + b } // 변수 선언부가 있는 경우 표현식의 자료형 생략
+var sumInt2 = { a: Int, b: Int -> a + b } // 변수 선언부가 생략된 경우에는 표현식에 자료형 표기
