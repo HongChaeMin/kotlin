3e3626b83ef90eaec3490e4acefad8795e9ff2d4
Hannah <ghdcoalss33@gmail.com>
2022-08-27 15:18:53 +0900

    feat(do-it-kotlin): chapter 8 - 1 upload

--- HelloKotlin/src/main/kotlin/chapter8/GenericBox.kt
+++ HelloKotlin/src/main/kotlin/chapter8/GenericBox.kt
@@ -1,15 +1,12 @@
 package chapter8
 
-import chapter8.limit.Box
-
 class Box<T>(t: T) { // 형식 매개변수로 받은 인자를 name에 저장
     var name = t
 }
 
 fun main() {
-    val box1: Box<Int> = chapter8.limit.Box<Int>(1) // 1은 Int 형이므로 Box<Int> 로 추록
-    val box2: Box<String> =
-        chapter8.limit.Box<String>("Hello") // Hello 는 String 형으로 Box<String> 으로 추론
+    val box1: Box<Int> = Box(1) // 1은 Int 형이므로 Box<Int> 로 추록
+    val box2: Box<String> = Box<String>("Hello") // Hello 는 String 형으로 Box<String> 으로 추론
 
     println(box1.name)
     println(box2.name)
